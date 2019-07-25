99bc3db1f5f0ff686d0e727be4f685bc5f8b0eac
Mihran <mihran.sahakyan@polymorphic.io>
2019-07-25 15:24:58 +0400

    COMPLETED: task-2

--- hwk-4/task-2/main.js
+++ hwk-4/task-2/main.js
@@ -1,46 +1,55 @@
 const promisify = require('promisify-mirshk');
 const fs = require('fs');
 
-class FileTree {
+class FileStructure {
     constructor() {
         this.readdirSync = promisify(fs.readdir);
+        this.depth = 1;
+        this.splitter = "-------";
+        this.modeIsTree = false;
     }
 
-     async * readDirSync(dir) {
+    async * readDirSync(dir, depth = this.depth) {
          const subFiles = await this.readdirSync(dir, { withFileTypes: true });
          for (const sf of subFiles) {
              if (sf.isDirectory()) {
-                 yield `${dir}/${sf.name}`;
-                 yield *this.readDirSync(`${dir}/${sf.name}`);
+                 yield this.modeIsTree ? `${this.splitter.repeat(depth)}# ${sf.name}` : `${dir}/${sf.name}`;
+                 yield *this.readDirSync(`${dir}/${sf.name}`, depth+1,);
              } else {
-                 yield `${dir}/${sf.name}`;
+                 yield this.modeIsTree ? `${this.splitter.repeat(depth)}* ${sf.name}` : `${dir}/${sf.name}`;
              }
          }
     }
 
-    printDirAsync(iterable) {
+    print(iterable) {
         const paths = [];
         (async () => {
             for await (const i of iterable) {
-                paths.push(i);
+                this.modeIsTree ? console.log(i) : paths.push(i);
             }
 
-            paths.sort((a, b) => {
-                let p1 = a.slice(0, a.lastIndexOf("/"));
-                let p2 = b.slice(0, b.lastIndexOf("/"));
-                return p1.length - p2.length;
-            });
-
-            console.log(paths);
-            return paths;
+            if (!this.modeIsTree) {
+                console.log(paths);
+                return paths;
+            }
         })();
     }
 
-
+    setMode(mode) {
+        if (typeof mode !== "boolean") {
+            throw Error("File Structure mode must have @boolean type");
+        } else {
+            this.modeIsTree = mode;
+            return this;
+        }
+    }
 }
 
-const ft = new FileTree();
-const dirs = ft.readDirSync(__dirname);
-ft.printDirAsync(dirs);
+const fstr = new FileStructure();
+const dirs = fstr.setMode(true).readDirSync(__dirname, true);
+fstr.print(dirs);
+
+
+
 
 
