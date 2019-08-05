cc424ee7e0c399447dc9894a2307eb5cbb2a6d55
MirShk <mihran.sahakyan.99@inbox.ru>
2019-08-06 00:05:01 +0400

    refactores(refined) variable, class and function names

--- hwk-4/task-2/main.js
+++ hwk-4/task-2/main.js
@@ -1,47 +1,35 @@
 const promisify = require('promisify-mirshk');
 const fs = require('fs');
 
-class FileStructure {
+class FileSystem {
     constructor() {
         this.readdirSync = promisify(fs.readdir);
-        this.splitter = "-------";
+        this.decorator = "-------";
         this.modeIsTree = false;
     }
 
-    async * readDir(dir, depth = 0) {
-         const subFiles = await this.readdirSync(dir, { withFileTypes: true });
-         for (const sf of subFiles) {
-             if (sf.isDirectory()) {
-                 yield this.modeIsTree ? `${this.splitter.repeat(depth)}|__ ${sf.name}` : `${dir}/${sf.name}`;
-                 yield *this.readDir(`${dir}/${sf.name}`, depth+1);
+    async * readFiles(dirName, fileHierarchyDepth = 0) {
+         const subFiles = await this.readdirSync(dirName, { withFileTypes: true });
+         for (const subFile of subFiles) {
+             const decoratorLength = this.decorator.repeat(fileHierarchyDepth);
+             if (subFile.isDirectory()) {
+                 yield this.modeIsTree ? `${decoratorLength}|__ ${subFile.name}` : `${dirName}/${subFile.name}`;
+                 yield *this.readFiles(`${dirName}/${subFile.name}`, fileHierarchyDepth + 1);
              } else {
-                 yield this.modeIsTree ? `${this.splitter.repeat(depth)}|__/ ${sf.name}` : `${dir}/${sf.name}`;
+                 yield this.modeIsTree ? `${decoratorLength}|__/ ${subFile.name}` : `${dirName}/${subFile.name}`;
              }
          }
-    }
-
-    async print(iterable) {
-        const paths = [];
-        for await (const i of iterable) {
-            this.modeIsTree ? console.log(i) : paths.push(i);
-        }
-
-        if (!this.modeIsTree) {
-            console.log(paths);
-            return paths;
-        }
     }
 
     /**
      *
      * @param mode
-     * @desc
-     * if the value of 'mode' param is true,
-     * the 'print' method will print file structure as a tree,
-     * else will print array of file paths
-     * @returns {FileStructure}
+     * @desc If the value of 'mode' param is true,
+     * the 'displayFileHierarchy' method will display file hierarchy tree,
+     * else will display an array of file paths.
+     * @returns {FileSystem}
      */
-    setPrintMode(mode) {
+    setDisplayMode(mode) {
         if (typeof mode !== "boolean") {
             throw new Error("FileStructure::setMode method receives only @boolean type argument!");
         } else {
@@ -49,14 +37,24 @@ class FileStructure {
             return this;
         }
     }
+
+    async displayFileHierarchy(iterable) {
+        const paths = [];
+        try {
+            for await (const it of iterable) this.modeIsTree ? console.log(it) : paths.push(it);
+            if (!this.modeIsTree) console.log(paths)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
 
-const fstr = new FileStructure();
-fstr.setPrintMode(true);
-fstr.print(fstr.readDir(__dirname));
-
-module.exports = new FileStructure();
-
+const fileSystem = new FileSystem();
+fileSystem
+        .setDisplayMode(true)
+        .displayFileHierarchy(
+            fileSystem.readFiles(__dirname)
+        );
 
 
 
